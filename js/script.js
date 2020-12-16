5f367bd58f16e326a1f761f661e63634f124e971
adele <adele.r87@gmail.com>
2020-12-16 15:48:45 +0100

    ordine alfabetico

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@
 var cognome = prompt('qual\è il tuo cognome?')
 // creo l array con i cognomi dati
 var listaCognomi =['Bianchi' , 'Rossi' , 'Duzioni' , 'Balsano' , 'Verdi'];
-// il sort mette in ordine alfabetico l array
-listaCognomi.sort();
+
 // creo la variabile da inserire nell html
 var output = document.getElementById('lista');
 // aggiungo il nuovo cognome nella lista con push
 listaCognomi.push(cognome);
+// il sort mette in ordine alfabetico l array ed è da inserire dopo averlo aggiunto
+listaCognomi.sort();
+// indexof per vedere la posizione umana della lista in cui il nuovo utente si trova
+listaCognomi.indexOf("cognome");
 // lo voglio vedere stampato sull html utilizzo l inner
 output.innerHTML=listaCognomi
 
@@ -26,7 +29,9 @@ for (i = 0; i < listaCognomi.length; i++ ){
 contenutoPrecedente = output.innerHTML;
 output.innerHTML = contenutoPrecedente + '<li>' + listaCognomi[i] + '</li>';
 
-
 }
+
+
+
 // alternativa alla riga 24
 // output.innerHTML + = '<li>' + listaCognomi[i] + '</li>'
